a307874cc60d6c8f914fa5d4ca2739a35a9a81b2
Clayton <clayton.cox@gmail.com>
2018-09-07 14:52:37 -0400

    Testing

--- Unit1.1/Program.cs
+++ Unit1.1/Program.cs
@@ -12,7 +12,7 @@ namespace Unit1._1
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 

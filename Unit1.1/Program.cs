6c120ee41c2f84f1dd79cbf5b7355ed90510c598
Clayton <clayton.cox@gmail.com>
2018-09-07 20:54:58 -0400

    first run

--- Unit1.1/Program.cs
+++ Unit1.1/Program.cs
@@ -10,12 +10,56 @@ namespace Unit1._1
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Test");
+            Console.WriteLine("Please Enter a three digit number: ");
+            var userNumber1 =(Console.ReadLine());
+
+            if (NumberCheck(userNumber1) == true && )
+                Console.WriteLine("Please Enter a 2nd three digit number");
+                var userNumber2 = (Console.ReadLine());
+
+
+
+            PlaceSumTotal(userNumber1, userNumber2);
+
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
-    }
+
+        public static bool NumberCheck(string x)
+        {
+            var testLenght = x.Length;
+            if (testLenght > 0 && testLenght < 4)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IntagerCheck()
+        {
+            return;
+        }
+
+        public static string PlaceSumTotal (string numberOne, string numberTwo)
+            {
+            char[] groupOne = numberOne.ToCharArray();
+            char[] groupTwo = numberOne.ToCharArray();
+
+            foreach (char c in groupOne)
+
+                groupOne[] +
+                
+
+
+            return;
+
+            }
+                                       
+
+         
+}
+
 }
